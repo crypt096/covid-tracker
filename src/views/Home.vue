1b29a83c9096585d0131a577fb6dc229c6123bc0
crypt0 <acvjetan@gmail.com>
2021-04-25 21:18:36 +0200

    Fetching data.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,21 @@
 <template>
-  <div class="home">
-    Hello world
-  </div>
+  <div class="home">Hello world</div>
 </template>
 
 <script>
 export default {
   name: "Home",
-  components: {
+  components: {},
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch("https://api.covid19api.com/summary");
+      const data = await res.json();
+      return data;
+    },
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+    console.log(data);
   },
 };
 </script>
