9028bba363051ca7b42d231a9b38d9e7d29e0921
Aleksandar Cvjetean <31933549+crypt096@users.noreply.github.com>
2021-08-02 16:58:10 +0200

    Add rest of code.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,13 @@
 <template>
   <header class="text-center bg-blue-800 text-white p-4 mb-10">
     <div class="text-3xl md:text-5xl font-bold mb-3">
-      <i class="fa fa-viruses"></i>Covid-19 Tracker
+      <i class="fa fa-viruses"></i> Covid-19 Tracker
     </div>
+
     <p>
       API by
-      <a class="text-blue-300" href="https://covid19api.com" target="_blank">
-        covid19api.com</a
+      <a class="text-blue-300" href="https://covid19api.com" target="_blank"
+      >covid19api.com</a
       >
     </p>
   </header>
@@ -14,9 +15,6 @@
 
 <script>
 export default {
-  name: "Header",
+  name: 'Header'
 };
 </script>
-
-<style>
-</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  purge: { content: ["./public/**/*.html", "./src/**/*.vue"] },
+  purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
     container: {
-      center: 'true',
-      padding: '2rem'
-    }
+      center: true,
+      padding: '2rem',
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-};
+}
